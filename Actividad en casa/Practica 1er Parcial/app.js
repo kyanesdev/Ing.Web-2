b9228f9bae580e706f10e4b5641bb44d87f3ad32
kyanesdev <kyanesdev@gmail.com>
2022-05-01 20:12:15 -0300

    Falta corregir funcion eliminar

--- Actividad en casa/Practica 1er Parcial/app.js
+++ Actividad en casa/Practica 1er Parcial/app.js
@@ -3,9 +3,13 @@ let emailRegister = document.getElementById("emailRegister");
 let passwordRegister = document.getElementById("passwordRegister");
 let perfil = document.getElementById("perfil");
 let formularioRegistro = document.getElementsByName("formularioRegistro");
+let formularioLogin = document.getElementsByName("formularioRegistro");
 
+let usuarioLogin = document.getElementById("usuarioLogin");
+let emailLogin = document.getElementById("emailLogin");
+let passwordLogin = document.getElementById("passwordLogin");
 
-
+let tablaUsuarios = document.getElementById("tablaUsuarios");
 
 function favTutorial() {  
     var mylist = document.getElementById("myList");  
@@ -20,10 +24,16 @@ function validarFormulario(formularioRegistro) {
     
 }
 
-const verificarVacio = ()=>{
+document.formularioLogin.addEventListener('submit', validarFormulario);
+function validarFormularioLogin(formularioLogin) {
+
+    formularioLogin.preventDefault(); //Evita el envío del formulario hasta comprobar
+    
+}
+const verificarVacioRegister = ()=>{
         let flag=false;
 
-        if(usuarioRegister.value == "" | emailRegister.value == "" | passwordRegister.value == "" | perfil.value == ""){
+        if(usuarioRegister.value == "" | emailRegister.value == "" | passwordRegister.value == ""){
             if(usuarioRegister.value == ""){
                 flag = true;
                 usuarioRegister.style.border = "1px solid red";
@@ -38,26 +48,73 @@ const verificarVacio = ()=>{
                 flag = true;
                 passwordRegister.style.border = "1px solid red";
             }
-        
-            if(perfil.value == ""){
-                flag = true;
-                perfil.style.border = "1px solid red";
-            }
+    
             validarFormulario(formularioRegistro);
             return flag
         }else{
             usuarioRegister.style.border = "1px solid green";
             emailRegister.style.border = "1px solid green";
             passwordRegister.style.border = "1px solid green";
-            perfil.style.border = "1px solid green";
         }
 }
 
+const verificarVacioLogin = ()=>{
+    let flag=false;
 
-const guardarInformacion = ()=>{
+    if(usuarioLogin.value == "" | emailLogin.value == "" | passwordLogin.value == ""){
+        if(usuarioRegister.value == ""){
+            flag = true;
+            usuarioLogin.style.border = "1px solid red";
+        }
     
+        if(emailLogin.value == ""){
+            flag = true;
+            emailLogin.style.border = "1px solid red";
+        }
+    
+        if(passwordLogin.value == ""){
+            flag = true;
+            passwordLogin.style.border = "1px solid red";
+        }
+
+        validarFormularioLogin(formularioLogin);
+        return flag
+    }else{
+        usuarioRegister.style.border = "1px solid green";
+        emailRegister.style.border = "1px solid green";
+        passwordRegister.style.border = "1px solid green";
+    }
+}
+
+let ocultarPerfilRegistro = document.getElementById("perfilRegistro").style.display = "none";
+const desplegarPerfilRegistro = ()=>{
+    document.getElementById("perfilRegistro").style.display = "block";
+}
+const guardarInformacion = () => {
     let ide = 1;
 
+    if(window.localStorage.getItem("id")){
+        ide = parseInt(window.localStorage.getItem("id"));
+    }
+
+    const persona = {
+        usuario: usuarioRegister.value,
+        email: emailRegister.value,
+        password: passwordRegister.value,
+        perfil: "Estandar",
+        id: ide
+    }
+
+    window.localStorage.setItem("NewUser"+ide, JSON.stringify(persona));
+    window.localStorage.setItem("id",ide+1);
+    
+}
+
+
+const guardarInformacionAdministrador = ()=>{
+    
+    let ide = 1;
+    desplegarPerfilRegistro();
     if(window.localStorage.getItem("id")){
         ide = parseInt(window.localStorage.getItem("id"));
     }
@@ -97,38 +154,37 @@ const verificarRegistroDuplicado = ()=>{
 }
 
 const buscarInformacion = ()=>{
-    let ide = 0;
+    let ide = 1;
 
     if(window.localStorage.getItem("id")){
         ide = parseInt(window.localStorage.getItem("id"));
     }else{
-        ide = 1
         window.localStorage.setItem("id",ide);
     }
-
+    
     for(let i = 1; i <= ide; i++){
         
         let persona = JSON.parse(window.localStorage.getItem("NewUser"+i));
-        if(perfil.value == "Estandar"){
-            if(persona.usuario == usuarioRegister.value && persona.password == passwordRegister.value){
-                alert(`Bienvenido ${persona.usuario}`);
+        
+            if(persona.usuario === usuarioLogin.value && persona.password === passwordLogin.value && persona.perfil === "Estandar"){
+                alert(`Bienvenido estandar: ${persona.usuario}`);
                 window.location.href = "./index.html";
             }
-        }if(perfil.value == "Avanzado"){
-            if(persona.usuario == usuarioRegister.value && persona.password == passwordRegister.value){
-                alert(`Bienvenido ${persona.usuario}`);
+           
+            if(persona.usuario === usuarioLogin.value && persona.password === passwordLogin.value && persona.perfil === "Avanzado"){
+                alert(`Bienvenido avanzado: ${persona.usuario}`);
                 window.location.href = "./index.html";
             }
-        }
-        if(perfil.value == "Administrador"){
-            if(persona.usuario == usuarioRegister.value && persona.password == passwordRegister.value){
+        
+            if(persona.usuario === usuarioLogin.value && persona.password === passwordLogin.value && persona.perfil === "Administrador"){
                 alert(`Bienvenido admin: ${persona.usuario}`);
+                desplegarPerfilRegistro();
+                desplegarMostrarTabla();
+                formularioLogin.preventDefault();
                 window.location.href = "./index.html";
                 //Falta hacer que una vez desplegado el admin, se le muestre la tabla de todos los usuarios
-                //Falta hacer que el admin solo el pueda registrar con nivel de perfil
-                //Por otra parte solo los estandar y avanzados pueden registrarse pero no cambiar el nivel de perfil
             }
-        }
+        
         
     }
     console.log(perfil.value)
@@ -139,8 +195,71 @@ const desplegarFormularioRegistro = ()=>{
     document.getElementById("opcionRegistro").style.display = "block";
 }
 
+const eliminarUsuario = ()=>{
+    
+    let ide;
+
+    if(window.localStorage.getItem("id")){
+        ide = parseInt(window.localStorage.getItem("id"));
+    }
+    for(let i = 1; i < ide; i++){
+        window.localStorage.removeItem("NewUser"+ide);
+    }
+    
+}
+
+const generarTabla = ()=>{
+    eliminarUsuario();
+    let ide;
+    if(window.localStorage.getItem("id")){
+        ide = parseInt(window.localStorage.getItem("id"));
+    }else{
+        ide = 1
+        window.localStorage.setItem("id",ide);
+    }
+
+    for(let i = 1; i <= ide; i++){
+        let persona = JSON.parse(window.localStorage.getItem("NewUser"+i));
+        let tabla = document.getElementById("tablaUsuarios");
+        let tr = document.createElement("tr");
+        let tdUsuario = document.createElement("td");
+        let tdEmail = document.createElement("td");
+        let tdPerfil = document.createElement("td");
+        let tdEliminar = document.createElement("td");
+        let btnEliminar = document.createElement("button");
+        
+
+        btnEliminar.innerHTML = "Eliminar";
+        btnEliminar.setAttribute("class","btn btn-danger");
+        btnEliminar.setAttribute("id","botonEliminar");
+        
+        btnEliminar.setAttribute("onclick","eliminarUsuario()");
+
+        tdUsuario.innerHTML = persona.usuario;
+        tdUsuario.setAttribute("id","usuarioTabla");
+        tdEmail.setAttribute("id","emailTabla");
+        tdPerfil.setAttribute("id","perfilTabla");
+
+        tdEmail.innerHTML = persona.email;
+        tdPerfil.innerHTML = persona.perfil;
+        tdEliminar.appendChild(btnEliminar);
+        tr.appendChild(tdUsuario);
+        tr.appendChild(tdEmail);
+        tr.appendChild(tdPerfil);
+        tr.appendChild(tdEliminar);
+        tabla.appendChild(tr);
+    }
+
+}
+tablaUsuarios = document.getElementById("tablaUsuarios").style.display = "none";
+const tabla = ()=>{
+    document.getElementById("tablaUsuarios").style.display = "block";
+    tablaUsuarios.innerHTML = "";
+    generarTabla();
+}
+
 const loguearse = ()=>{
-    if(verificarVacio()){
+    if(verificarVacioLogin()){
         return
     }else{
         buscarInformacion();
@@ -149,7 +268,7 @@ const loguearse = ()=>{
 
 function registro(){
 
-        if(verificarVacio()){
+        if(verificarVacioRegister()){
             return
         }
         if(verificarRegistroDuplicado()){
@@ -162,3 +281,9 @@ function registro(){
 function login(){
     loguearse();
 }
+
+let mostrarTabla = document.getElementById("mostrarTabla").style.display = "none";
+const desplegarMostrarTabla = ()=>{
+    document.getElementById("mostrarTabla").style.display = "block";
+}
+
